7eff3383c3251e89feb588023eb7fd925153c0f3
Hung Nguyen <sinhhung.ng@gmail.com>
2025-08-12 21:40:35 +0700

    LeetCode/12-08-2025-valid-palindrome: optimize

--- LeetCode/String/ValidPalindrome.java
+++ LeetCode/String/ValidPalindrome.java
@@ -2,31 +2,52 @@
 
 public class ValidPalindrome {
     public boolean  isPalindrome(String s) {
-        char[] cha = s.trim().toLowerCase().toCharArray();
-        if(cha.length == 0){return true;}
-        String temp = new String();
-        for (int i = 0; i < cha.length; i++) {
-            if(Character.isLetterOrDigit(cha[i])){
-                temp = temp.concat(Character.toString(cha[i]));
+        char[] cha = s.toCharArray();
+        int i = 0, j = cha.length - 1;
+        while(i <= j){
+            if(!Character.isLetterOrDigit(cha[i])){
+                i++;
+                continue;
             }
-        }
-        
-        char [] chaTemp = temp.toCharArray();
-        int i = 0;
-        int j = chaTemp.length - 1;
-        while (i < j) { 
-            if(chaTemp[i] != chaTemp[j]){
+
+            if(!Character.isLetterOrDigit(cha[j])){
+                j--;
+                continue;
+            }
+
+            if(Character.toLowerCase(cha[i]) != Character.toLowerCase(cha[j])){
                 return false;
             }
             i++;
             j--;
+
+
+
+
+
+            // if(Character.isLetterOrDigit(cha[i]) && Character.isLetterOrDigit(cha[j])){
+            //     char left = Character.isUpperCase(cha[i]) ? Character.toLowerCase(cha[i]) : cha[i];
+            //     char right = Character.isUpperCase(cha[j]) ? Character.toLowerCase(cha[j]) : cha[j];
+            //     if(left != right){
+            //         return false;
+            //     }
+            //     i++;
+            //     j--;
+            // } else {
+            //     if(!Character.isLetterOrDigit(cha[i])){
+            //         i++;
+            //     } else {
+            //         j--;
+            //     }
+            // }
         }
         return true;
     }
 
     public static void main(String[] args) {
         ValidPalindrome vp = new ValidPalindrome();
-        System.out.println(vp.isPalindrome(" "));
+        System.out.println(vp.isPalindrome("race a car"));
+        // System.out.println(vp.isPalindrome("A man, a plan, a canal: Panama"));
     }
 
 }
